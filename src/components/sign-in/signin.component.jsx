8d4fe8842a0f9fabba90290c26ad851972aa8fb4
Changretta <johnylaf94@gmail.com>
2019-12-02 13:30:54 +0100

    set up redux library and did the config

--- src/components/sign-in/signin.component.jsx
+++ src/components/sign-in/signin.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './signin.scss';
 import FormInput from '../form-input/form-input.component'
 import CustomButtom from '../custom-button/custom.button.component'
-import { signInWithGoogle} from '../../firebase/firebase.utils'
+import { auth , signInWithGoogle} from '../../firebase/firebase.utils'
 
 
 class SignIn extends React.Component{
@@ -16,8 +16,19 @@ class SignIn extends React.Component{
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
+
+        const { email , password } = this.state;
+
+        try{
+            await auth.signInWithEmailAndPassword(email,password);
+            this.setState({ email: '' ,password: '' })
+        }catch (error){
+            console.log(error); 
+        }
+
+
 
         this.setState({email:'',password: ''})
     }
@@ -26,7 +37,7 @@ class SignIn extends React.Component{
     handleChange = event => {
         const { value , name } = event.target;
 
-        this.setState = ({[name]: value })
+        this.setState({[name]: value })
     }
 
     render(){
